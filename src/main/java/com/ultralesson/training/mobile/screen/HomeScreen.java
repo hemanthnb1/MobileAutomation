1eb2c2797ccd37a78dbd27889df109a4b3f72563
hemanthnb1 <hemanthn@testvagrant.com>
2023-08-21 12:13:43 +0530

    Third comit

--- src/main/java/com/ultralesson/training/mobile/screen/HomeScreen.java
+++ src/main/java/com/ultralesson/training/mobile/screen/HomeScreen.java
@@ -6,11 +6,33 @@ public class HomeScreen {
 
     private AppiumDriver appiumDriver;
 
+    private By assignments = By.xpath("//*[@text='Assignments']");
+    private By notes = By.xpath("//*[@text='Notes']");
+    private By resources = By.xpath("//*[@text='Resources']");
+    private By profile = By.xpath("//*[@text='Profile']");
+
+
     public HomeScreen(AppiumDriver appiumDriver) {
         this.appiumDriver = appiumDriver;
     }
 
     public ProfileScreen navToProfile() {
+        appiumDriver.findElement(profile).click();
+        return new ProfileScreen(appiumDriver);
+    }
+
+    public ProfileScreen navToAssignments() {
+        appiumDriver.findElement(assignments).click();
+        return new ProfileScreen(appiumDriver);
+    }
+
+    public ProfileScreen navToNotes() {
+        appiumDriver.findElement(notes).click();
+        return new ProfileScreen(appiumDriver);
+    }
+
+    public ProfileScreen navToResources() {
+        appiumDriver.findElement(resources).click();
         return new ProfileScreen(appiumDriver);
     }
 }
